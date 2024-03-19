37709b95a9411a2022c1af24d89f711355469549
Sarah German <sgerman@gitlab.com>
2024-03-19 00:16:48 +0000

    Add Vue tooling

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,5 @@ import defaultPreset from "@gitlab/ui/tailwind.defaults";
 /** @type {import('tailwindcss').Config} */
 export default {
   presets: [defaultPreset],
-  content: ["themes/**/*.html"],
+  content: ["themes/**/*.html", "themes/**/*.vue"],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
+import { globSync } from "glob";
 import copy from "rollup-plugin-copy";
+import vue2 from "@vitejs/plugin-vue2";
+import resolve from "@rollup/plugin-node-resolve";
+import fs from "fs";
+import path from "path";
 
 const THEME_PATH = "themes/gitlab-docs";
 const THEME_VENDOR_DIR = `${THEME_PATH}/static/gitlab_ui`;
@@ -11,16 +16,23 @@ const GITLAB_THEME_ASSETS = [
   "@gitlab/svgs/dist/sprite_icons/*",
 ];
 
+// Process all JS files in the theme src directory.
+// See https://rollupjs.org/configuration-options/#input
+const inputs = globSync(`${THEME_PATH}/src/*.js`).reduce((entries, file) => {
+  const name = path.basename(file);
+  return { ...entries, [name]: file };
+}, {});
+
 export default defineConfig({
   publicDir: false,
   build: {
     outDir: `${THEME_PATH}/static/vite`,
     rollupOptions: {
-      input: {
-        main: `${THEME_PATH}/src/main.js`,
-      },
+      input: inputs,
       output: {
-        entryFileNames: "[name].js",
+        format: "es",
+        inlineDynamicImports: false,
+        entryFileNames: "[name]",
         assetFileNames: `[name].[ext]`,
       },
       plugins: [
@@ -33,7 +45,40 @@ export default defineConfig({
             };
           }),
         }),
+        vue2(),
+        resolve(),
       ],
+      onwarn(warning, warn) {
+        // Ignore known issues from third-party code.
+        const ignore = {
+          INVALID_ANNOTATION: ["bootstrap-vue"],
+        };
+        const ignoredKeywords = ignore[warning.code] || [];
+        if (
+          !ignoredKeywords.some((keyword) => warning.message.includes(keyword))
+        ) {
+          warn(warning);
+        }
+      },
     },
   },
+  plugins: [
+    {
+      // Move icons.svg from GitLab UI out of the Vite directory.
+      // Otherwise it will 404 when viewing components with icons.
+      name: "move-gitlab-ui-icons-svg",
+      closeBundle: () => {
+        const sourcePath = path.join(THEME_PATH, "static/vite/icons.svg");
+        const destinationPath = path.join(THEME_PATH, "static/icons.svg");
+
+        if (fs.existsSync(sourcePath)) {
+          fs.rename(sourcePath, destinationPath, (err) => {
+            if (err) {
+              console.error("Error moving icons.svg:", err);
+            }
+          });
+        }
+      },
+    },
+  ],
 });
