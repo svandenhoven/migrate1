6503200b8bb53d9460ff4b6055e2771415a434d1
Sarah German <sgerman@gitlab.com>
2024-05-07 17:32:42 +0000

    Add script to copy and modify navigation.yaml

--- scripts/content-post-process.sh
+++ scripts/content-post-process.sh
@@ -21,9 +21,13 @@ for ENTRY in $(yq eval '.products | keys | .[]' "$PRODUCTS_YAML"); do
     printf "Renaming index files in ${CLONE_DIR}...\n"
     find "$DOCS_PATH" -type f -name 'index.md' -exec sh -c 'mv "$1" "${1%/*}/_index.md"' _ {} \;
 
-    # Run all migration scripts
+    # Run content migration scripts
     printf "Updating content in ${CLONE_DIR}...\n"
     go run cmd/gldocs/main.go migrate "$DOCS_PATH"
 done
+
+# Fetch YAML data files
+printf "Fetching YAML data files...\n"
+go run cmd/gldocs/main.go fetch
 
 printf "INFO: Content updates complete!\n"
